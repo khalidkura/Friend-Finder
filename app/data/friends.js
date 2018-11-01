aa0eb90d2c96554dcf1efb8f37de93c49c67423d
khalidkura <khalidkura@gmail.com>
2018-10-31 23:02:42 -0400

    final upload

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friendsData = [
+    {
+      name: "Dwayne Johnson",
+      photo: "https://shop.wwe.com/on/demandware.static/-/Sites/default/dw29757933/images/slot/landing/superstar-landing/Superstar-Category_Superstar_562x408_theRock.png",
+      scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+      name: "Jamal Crawford",
+      photo:"http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/165.png&w=350&h=254",
+      scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+      name: "Tina Fey",
+      photo:"https://s.abcnews.com/images/Entertainment/tina-fey-gty-jt-180505_hpMain_4x3_992.jpg",
+      scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+      name: "Michael Jackson",
+      photo:"https://ichef.bbci.co.uk/images/ic/960x540/p01bqlx8.jpg",
+      scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+      name: "Will Smith",
+      photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Will_Smith_by_Gage_Skidmore.jpg/220px-Will_Smith_by_Gage_Skidmore.jpg",
+      scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+    },
+    {
+      name: "Beyonce",
+      photo:"https://lh3.googleusercontent.com/xzSTC-P_mX0POVuwjUCuyzNYH-Y0SpJw9nB09ZqW4bJnLFFBGysoZ1SLgIU2nwB9kUeiANZgIqDDRdtVeCNI5Q=s750",
+      scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+    },
+    {
+      name: "Angelina Jolie",
+      photo:"https://s1.r29static.com//bin/entry/c98/720x864,80/1951955/image.jpg",
+      scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+    }
+  ];
+
+  module.exports = friendsData;
